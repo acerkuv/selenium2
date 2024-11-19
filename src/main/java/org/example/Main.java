0df191b4d3fad8d11c52f8412130e4c567fd5b7c
Eugene <47480436+acerkuv@users.noreply.github.com>
2024-11-19 12:36:28 +0300

    Update Main.java
    Удалили лишнеие комменты

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,7 +6,7 @@ import java.util.Map;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-//      ta:  "https://www.avito.ru/brands/i338763229/all/avtomobili?src=search_seller_info&sellerId=ebbe929fc98793a9837e611c17a5c95b"
+
 
         WebDrivingTwo webDrivingTwo = new WebDrivingTwo("https://www.avito.ru/brands/i195276908/all?src=search_seller_info&sellerId=9455326a5ed54207e344928caea60671");
         webDrivingTwo.runToParse();
